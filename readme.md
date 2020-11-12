9c77472c9c56211aa4bc44de439cc6d6992264a8
binch <bin.bc.chen@oracle.com>
2020-11-11 22:26:55 -0800

    added setup.py

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Install
+```
+pip -v install -e git+ssh://git@github.com/bincdianshangcun/pycommon.git@v0.0.1#egg=pycommon&subdirectory=src
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pycommon", # Replace with your own username
+    version="v0.0.2",
+    author="dianshangcun",
+    author_email="author@example.com",
+    description="A small pycommon package",
+    url="https://github.com:bincdianshangcun/pycommon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
